f963280432a75e6200cddaae3541f3e83f26b33a
DESKTOP-T3HL377\Bobol <lihl@legion-tech.net>
2021-10-14 03:58:34 +0800

    #1 深拷贝及深拷贝测例

---
+++ src/拷贝/深拷贝.js
@@ -0,0 +1,67 @@
+
+Object.prototype.for = function (cb) {
+    if (Array.isArray(this) || this instanceof Set) {
+        this.forEach(cb)
+    } else if (this instanceof Map) {
+        ([...this]).forEach(([key, val]) => cb(val, key))
+    }
+    else if (this instanceof Object) {
+        Object.entries(this).forEach(([key, val]) => cb(val, key))
+    }
+}
+
+
+// 只给 Object 原型链上添加 cloneDeep 方法，避免不合规定的数据使用
+// ALREADY_CLONED：已创建对象 避免无限引用 注意：会增大内存使用 并大幅降低性能
+Object.prototype.cloneDeep = function (CLONED_MAP = new WeakMap()) {
+
+    // 循环调用缓存区有目标值
+    if (CLONED_MAP.has(this)) return CLONED_MAP.get(this)
+
+    function _insert(parent, child, child_key) {
+        if (!parent) return
+
+        if (Array.isArray(parent)) {
+            parent[child_key] = child
+        }
+        // Map
+        else if (parent instanceof Map) {
+            parent.set(child_key, child)
+        }
+        // Set
+        else if (parent instanceof Set) {
+            parent.add(child)
+        }
+        // Object
+        else if (parent instanceof Object) {
+            parent[child_key] = child
+        }
+    }
+
+    // 克隆方法 递归入口
+    function _clone(data) {
+        return ['object', 'function'].includes(typeof data) ? data.cloneDeep(CLONED_MAP) : data
+    }
+
+
+    switch (typeof this) {
+        case 'object':
+            const new_obj = new this.constructor
+            CLONED_MAP.set(this, new_obj)
+            this.for((val, key) => {
+                _insert(new_obj, _clone(val), key)
+            })
+            return new_obj
+        case 'function':
+            eval(`var copy_fn = ${this.toString()}`) // 复制方法体
+
+            CLONED_MAP.set(this, copy_fn)
+            for (let arg_key in this) { // 复制方法体参数
+                if (!['for', 'cloneDeep'].includes(arg_key)) copy_fn[arg_key] = _clone(this[arg_key])
+            }
+            return copy_fn
+
+        default:
+            break;
+    }
+}
---
+++ test/深拷贝.test.js
@@ -0,0 +1,116 @@
+require('should')
+require('../src/拷贝/深拷贝')
+
+describe('复制单一类型', function () {
+    const 对比表 = {
+        对象: { a: 1, b: '2' },
+        Hash表: new Map([[1, 2], [2, 3]]),
+        数组: [1, '2'],
+        集合: new Set([1, '2', false]),
+        函数: function fn() { console.log('我是一个函数') }
+    }
+
+    Object.entries(对比表).forEach(([测例名, 数据]) => {
+        it(测例名, function () {
+            const origin = 数据
+            const target = origin.cloneDeep()
+
+            origin.should.not.equal(target) // === 对比
+            origin.should.eql(target) // 深度对比
+        })
+    })
+})
+
+describe('混合多种 Object 子类型', function () {
+    const 对比表 = {
+        混合: { a: 1, b: '2', c: [1, 2, { cc: 3, fn: function fn() { console.log('我是一个函数') } }, [4, 5], new Map([[1, 2], [2, [3]], ['3', { ccc: new Map([[6, 7]]) }]])] }
+    }
+
+    Object.entries(对比表).forEach(([测例名, 数据]) => {
+        it(测例名, function () {
+            const origin = 数据
+            const target = origin.cloneDeep()
+
+            origin.should.not.equal(target) // === 对比
+            origin.should.eql(target) // 深度对比
+
+            origin.c.should.not.equal(target) // === 对比
+            origin.c.should.eql(target.c) // 深度对比
+
+            origin.c[2].should.not.equal(target) // === 对比
+            origin.c[2].should.eql(target.c[2]) // 深度对比
+
+            origin.c[3].should.not.equal(target) // === 对比
+            origin.c[3].should.eql(target.c[3]) // 深度对比
+
+        })
+    })
+})
+
+describe('循环调用', function () {
+    // TODO 解一层循环 不应该在 _clone 中再检查、提取返回相同的circular
+    // 循环调用1
+    const array_key = ['ak']
+    const map = new Map()
+    const _map = new Map()
+    _map.set('f', { ff: 'fff' })
+    _map.set(array_key, [1, '2', { g: { gg: 'ggg' }, f: map }]) // map、_map 循环调用
+    map.set('c', 'cc')
+    map.set('d', 'dd')
+    map.set('e', _map)
+
+    // 循环调用2
+    const a = {}
+    const b = { a }
+    a.b = b
+
+    // 循环调用3
+    function fn(val) { console.log(val) }
+    fn.fn = fn
+
+    const 对比表 = {
+        // 循环调用1: [1, 2, [3, 4, [5], { num: 6 }], { aa: 1, b: [2, 3] }, map],
+        // 循环调用2: a,
+        循环调用3: fn
+    }
+
+    Object.entries(对比表).forEach(([测例名, 数据]) => {
+        it.only(测例名, function () {
+            const origin = 数据
+            const target = origin.cloneDeep()
+
+            origin.should.not.equal(target) // === 对比
+
+            switch (测例名) {
+                case '循环调用1':
+                    origin[4].should.equal(origin[4].get('e').get(array_key)[2].f)
+                    target[4].should.equal(target[4].get('e').get(array_key)[2].f)
+
+                    origin[4].should.not.equal(target[4].get('e').get(array_key)[2].f)
+                    origin[4].should.eql(target[4].get('e').get(array_key)[2].f)
+                    break;
+
+                case '循环调用2':
+                    origin.should.equal(origin.b.a)
+                    target.should.equal(target.b.a)
+
+                    origin.should.eql(target)
+                    origin.should.not.equal(target)
+                    break;
+
+                case '循环调用3':
+                    origin.should.equal(origin.fn)
+                    target.should.equal(target.fn)
+
+                    origin.should.eql(target)
+                    origin.should.not.equal(target)
+
+                default:
+
+                    break;
+            }
+
+
+        })
+    })
+})
